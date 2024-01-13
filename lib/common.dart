a0fa76197b316755ef156b05a04f8938d20357b1
leekina <leemh332@gmail.com>
2024-01-13 16:35:08 +0900

    add : add common data

---
+++ lib/common.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+//dfdfdf
+double contextWidth = Get.width;
+double contextHeight = Get.height;
+double kFHDHeight = 1080;
+double kFHDWidth = 1920;
+Color kMainColor = const Color(0xff033423);
