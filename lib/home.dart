6219562f001f43d81ad8dcf71b0b0fe7195a05fd
leekina <leemh332@gmail.com>
2024-01-14 13:24:03 +0900

    create : about view 정보 추가

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,10 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
+import 'package:flutter/material.dart';
+import 'package:portfolio_leeminhyeong/common.dart';
+import 'package:portfolio_leeminhyeong/components/custom_text.dart';
+
+import 'view/about_view.dart';
 import 'view/header.dart';
 
 class Home extends StatelessWidget {
@@ -7,9 +12,15 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    log('view width ${MediaQuery.of(context).size.width}');
     return Scaffold(
       body: SingleChildScrollView(
-        child: Header(),
+        child: Column(
+          children: [
+            Header(),
+            AboutView(),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/view/about_view.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio_leeminhyeong/common.dart';
+import 'package:portfolio_leeminhyeong/components/custom_text.dart';
+
+import '../info/info.dart';
+
+class AboutView extends StatelessWidget {
+  const AboutView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: kFHDWidth,
+      padding: EdgeInsets.symmetric(vertical: 120),
+      decoration: BoxDecoration(
+        color: Color(0xffF9F9F9),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextCustom.type('About', type: TextType.mainTitle),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'images/about_image.png',
+                width: contextWidth(context) * 0.4,
+              ),
+              SizedBox(width: 100),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: List.generate(
+                  about_info.length,
+                  (index) {
+                    return Column(
+                      children: [
+                        InfoDetail(index),
+                        SizedBox(height: 40),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Column InfoDetail(int index) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: List.generate(
+        about_info.elementAt(index).length,
+        (innerIndex) {
+          if (innerIndex == 0) {
+            return TextCustom.type(about_info.elementAt(index).first,
+                type: TextType.subTitle);
+          } else {
+            return TextCustom.type(
+                about_info.elementAt(index).elementAt(innerIndex),
+                type: TextType.text);
+          }
+        },
+      ),
+    );
+  }
+}
