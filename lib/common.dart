d933807d922fe70b799da65c5be82c54985ee9b2
leekina <leemh332@gmail.com>
2024-01-14 13:22:54 +0900

    Feat : 현재 화면사이즈 받아오는 함수 추가

--- lib/common.dart
+++ lib/common.dart
@@ -3,8 +3,16 @@ import 'package:get/get.dart';
 
 String kMainFont = 'AppleSDGothicNeo';
 String kTitleFont = 'Frank_Ruhl_Libre';
-double contextWidth = Get.width;
-double contextHeight = Get.height;
+// double contextWidth = Get.width;
+// double contextHeight = Get.height;
 double kFHDHeight = 1080;
 double kFHDWidth = 1920;
 Color kMainColor = const Color(0xff033423);
+
+double contextWidth(BuildContext context) {
+  return MediaQuery.of(context).size.width;
+}
+
+double contextHeight(BuildContext context) {
+  return MediaQuery.of(context).size.height;
+}
