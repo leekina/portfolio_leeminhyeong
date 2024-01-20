29b365677fcf7955f1cb66e9344cb4305e8d6ead
leekina <leemh332@gmail.com>
2024-01-20 15:17:20 +0900

    add : add custom flooting button

---
+++ lib/components/custom_flooting_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio_leeminhyeong/components/custom_text.dart';
+
+class CustomFlootingButton extends StatelessWidget {
+  const CustomFlootingButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      constraints: BoxConstraints.expand(
+        width: 200,
+        height: 80,
+      ),
+      fillColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(110),
+      ),
+      padding: EdgeInsets.all(15),
+      onPressed: () {
+        print('view Code');
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(40),
+            ),
+            child: Image.asset('assets/images/icon_github.png'),
+          ),
+          SizedBox(width: 8),
+          TextCustom.type('view code', type: TextType.text),
+        ],
+      ),
+    );
+  }
+}
