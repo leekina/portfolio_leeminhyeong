5ca0156669bfa2a3da87f048a2d6ad64b11aa9ea
lmh <leemh332@gmail.com>
2024-11-28 17:15:46 +0900

    remove comments

--- lib/main.dart
+++ lib/main.dart
@@ -29,13 +29,4 @@ class MyApp extends StatelessWidget {
       home: const HomePage(),
     );
   }
-
-  // Future _initImages() async {
-  //   // >> To get paths you need these 2 lines
-  //   final manifestContent = await rootBundle.loadString('AssetManifest.json');
-
-  //   final Map<String, dynamic> manifestMap = json.decode(manifestContent);
-  //   // >> To get paths you need these 2 lines
-  //   // print(manifestContent);
-  // }
 }
